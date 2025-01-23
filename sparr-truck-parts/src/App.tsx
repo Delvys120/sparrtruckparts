48612dfc37a7f4849dac1c80d26542b5d0532ba2
Delvys120 <Delvystech@gmail.com>
2025-01-23 11:16:26 -0500

    Delvys made changes to App.tsx, reportWebVitals.ts and package.json

--- sparr-truck-parts/src/App.tsx
+++ sparr-truck-parts/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@mui/material';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Sparr Truck Parts</h1>
+      <p>Welcome to the Sparr Truck Parts site.</p>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
     </div>
   );
-}
+};
 
 export default App;
+
