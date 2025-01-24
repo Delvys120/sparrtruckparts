45c5a3987987f6d56e303cdf5f4420d1847ac792
sly750 <calvarez1007@yahoo.com>
2025-01-23 20:19:43 -0500

    Added: theme/context/navbar

--- sparr-truck-parts/src/index.tsx
+++ sparr-truck-parts/src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals.ts';
+import { ThemeProvider } from '@mui/material'; 
+import { theme } from './Theme.ts';
+import { LanguageProvider } from './LanguageContext/LanguageContext.tsx';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <LanguageProvider>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+      </LanguageProvider>
   </React.StrictMode>
 );
 
